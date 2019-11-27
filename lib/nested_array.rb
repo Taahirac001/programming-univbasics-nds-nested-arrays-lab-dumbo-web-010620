a5e03a2f77833e26a667734cbfec2527e388b3ab
Taahira Carter <taahirac001@gmail.com>
2019-11-27 01:41:55 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -22,7 +22,7 @@ def assembled_matrix
 end
 
 def array_literal_matrix
-  new_array = [["Don Gately", "Joelle van Dyne", "Pat M.", "Kate", "Gompert", "Bruce Green"], [ "Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]]
+  new_array = [["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"], [ "Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]]
   new_array[0].sort!
   new_array[1].sort!
   new_array
